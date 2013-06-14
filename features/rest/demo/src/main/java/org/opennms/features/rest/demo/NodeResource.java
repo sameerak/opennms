28c0aed7441fbde1e350d61207958696227f1f97
sameerak <dlskannangara@gmail.com>
2013-06-15 02:08:04 +0530

    test search funtionality using org.opennms.core.criteria package

--- features/rest/demo/src/main/java/org/opennms/features/rest/demo/NodeResource.java
+++ features/rest/demo/src/main/java/org/opennms/features/rest/demo/NodeResource.java
@@ -1,5 +1,6 @@
 package org.opennms.features.rest.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,8 +8,15 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.opennms.core.criteria.Criteria;
+import org.opennms.core.criteria.CriteriaBuilder;
+import org.opennms.core.criteria.Order;
+import org.opennms.core.criteria.Alias.JoinType;
+import org.opennms.core.criteria.restrictions.Restriction;
+import org.opennms.core.criteria.restrictions.Restrictions;
 import org.opennms.netmgt.dao.NodeDao;
 import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.netmgt.model.OnmsNodeList;
 
 @Path("/nodes")
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -42,5 +50,37 @@ public class NodeResource {
      */
     public void setNodeDao(NodeDao nodeDao) {
         this.nodeDao = nodeDao;
+    }
+
+    /**
+     * method to test the criteria for node searching
+     * based on the NodeRestService of old REST API
+     * @return List<OnmsNode>
+     */
+    @GET
+    @Path("/test")
+    public List<OnmsNode> testSearch() {
+        final CriteriaBuilder builder = new CriteriaBuilder(OnmsNode.class);
+        builder.alias("snmpInterfaces", "snmpInterface", JoinType.LEFT_JOIN);
+        builder.alias("ipInterfaces", "ipInterface", JoinType.LEFT_JOIN);
+        builder.alias("categories", "category", JoinType.LEFT_JOIN);
+
+        builder.orderBy("label").asc();
+        
+        final Criteria crit = builder.toCriteria();
+
+        final List<Restriction> restrictions = new ArrayList<Restriction>(crit.getRestrictions());
+            restrictions.add(Restrictions.ne("type", "D"));
+            crit.setRestrictions(restrictions);
+        
+        final OnmsNodeList coll = new OnmsNodeList(nodeDao.findMatching(crit));
+        
+        crit.setLimit(null);
+        crit.setOffset(null);
+        crit.setOrders(new ArrayList<Order>());
+
+        coll.setTotalCount(nodeDao.countMatching(crit));
+
+        return coll;
     }
 }
