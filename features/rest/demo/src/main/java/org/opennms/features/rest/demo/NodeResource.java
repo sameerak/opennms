e25ed7429a4eec59e1a3eebbfaeb692ea2f88bca
sameerak <dlskannangara@gmail.com>
2013-06-22 22:54:51 +0530

    testing query parameter transmission

--- features/rest/demo/src/main/java/org/opennms/features/rest/demo/NodeResource.java
+++ features/rest/demo/src/main/java/org/opennms/features/rest/demo/NodeResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.opennms.netmgt.dao.NodeDao;
@@ -42,5 +43,17 @@ public class NodeResource {
      */
     public void setNodeDao(NodeDao nodeDao) {
         this.nodeDao = nodeDao;
+    }
+    
+    /**
+     * Testing the transmission of query parameters 
+     * 
+     * @param queryString
+     * @return
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String searchNodes(@QueryParam("_s") String queryString) {
+        return queryString;
     }
 }
