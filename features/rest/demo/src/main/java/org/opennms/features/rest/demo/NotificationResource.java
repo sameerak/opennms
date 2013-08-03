b6c9cd2d01f498e2df9c537c0ed277ad2bb50ad5
sameerak <dlskannangara@gmail.com>
2013-08-03 18:45:14 +0530

    adding search functionality to notification REST resource

--- features/rest/demo/src/main/java/org/opennms/features/rest/demo/NotificationResource.java
+++ features/rest/demo/src/main/java/org/opennms/features/rest/demo/NotificationResource.java
@@ -1,17 +1,30 @@
 package org.opennms.features.rest.demo;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.opennms.core.criteria.Criteria;
+import org.opennms.core.criteria.CriteriaBuilder;
+import org.opennms.features.rest.demo.exception.NotFIQLOperatorException;
+import org.opennms.features.rest.demo.util.QueryDecoder;
 import org.opennms.netmgt.dao.NotificationDao;
 import org.opennms.netmgt.model.OnmsNotification;
+import org.opennms.netmgt.model.OnmsNotificationCollection;
+import org.opennms.netmgt.model.OnmsSeverity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.orm.hibernate3.HibernateQueryException;
 
 @Path("/notifications")
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -38,4 +51,82 @@ public class NotificationResource {
     public void setNotificationDao(NotificationDao outageDao) {
         this.notificationDao = outageDao;
     }
+    
+    @GET
+    @Path("/search")
+    public Response searchNotifications(@QueryParam("_s") String queryString) {
+        try{
+            QueryDecoder nqd = new NotificationQueryDecoder();
+            Criteria crit = nqd.FIQLtoCriteria(queryString);
+            OnmsNotificationCollection result = new OnmsNotificationCollection(notificationDao.findMatching(crit));
+            if (result.isEmpty()) {         //result set is empty
+                return Response.noContent().build();
+            }
+            return Response.ok().entity(result).build();
+        }
+        catch(NotFIQLOperatorException e){    //in a case where user has specified an invalid FIQL operator
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(e.getMessage()).build();   
+        }
+        catch(ParseException e){    //in a case where user has provided data in wrong format
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(e.getMessage()).build();   
+        }
+        catch(HibernateQueryException e){    //in a case where user has requested a non existing data type
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(e.getMessage()).build();   
+        }
+        catch(Exception e){
+            logger.error(e.getMessage(), e);    
+            return Response.serverError().type(MediaType.TEXT_PLAIN).entity(e.getMessage()).build();   //in case of an unidentified error caused
+        }
+    }
+    
+    /**
+     * inner class to do the query decoding part of the search function 
+     *
+     */
+    private class NotificationQueryDecoder extends QueryDecoder { //start of inner class
+        
+        /**
+         * implemented abstract method from QueryDecoder class
+         * in order to create the appropriate criteria object
+         * 
+         */
+        protected Criteria CreateCriteria(){
+            final CriteriaBuilder builder = new CriteriaBuilder(OnmsNotification.class);
+            
+            builder.orderBy("notifyId").desc();
+            
+            final Criteria crit = builder.toCriteria();
+            
+            return crit;
+        }
+        
+        /**
+         * implemented abstract method from QueryDecoder class
+         * For the given property name respective comparable object is created
+         * ex - createTime -> java.util.Date
+         * 
+         * TODO - extend to provide validations
+         * 
+         * @param propertyName
+         * @param compareValue
+         * @return
+         * @throws ParseException 
+         */
+        protected Object getCompareObject(String propertyName, String compareValue) throws ParseException {
+            if (propertyName.equals("pageTime") || propertyName.equals("respondTime")) {
+                DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                try {
+                    Date formattedDate = formatter.parse(compareValue);
+                    return formattedDate;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    throw new ParseException("Please specify dates in format \"yyyy-MM-dd'T'HH:mm:ss\"", 0);
+                }
+            }
+            else if (propertyName.equals("id")) {
+                return Integer.parseInt(compareValue);
+            }
+            return compareValue;
+        }
+    }//end of inner class
 }
